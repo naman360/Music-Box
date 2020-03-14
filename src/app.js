21fede3ccf5d0310d8b146a452399eb682392d11
Naman Shankhydhar <naman.geek@gmail.com>
2020-03-14 09:35:33 +0530

    Added support for mobile devices

--- src/app.js
+++ src/app.js
@@ -18,9 +18,26 @@ function onKeyDown(event){          //Keydown event listener
     audioData[event.key].sound.play();
     
 }
-
-
 }
+
+// var circles=[];
+
+document.getElementById('myCanvas').addEventListener('click',function(){ 
+    var nums=Math.floor(Math.random()*27);
+    var playAudio=keyData[nums].pressKey;           //Used [] for Objects of objects
+
+    pointX=Math.random()*point.x;
+    pointY=Math.random()*point.y;
+    
+    // Creating a circle
+    var newCircle=new Path.Circle(pointX,pointY,500)
+    newCircle.fillColor="red";
+    circles.push(newCircle);
+    newCircle.fillColor=audioData[playAudio].color;
+    audioData[playAudio].sound.play();
+});
+
+
 
 function onFrame(event){            //For animation of circle array and change of hue.
     for(var i=0;i<circles.length;i++){
@@ -28,3 +45,4 @@ function onFrame(event){            //For animation of circle array and change o
     circles[i].scale(.9);
     }
 }
+
---
+++ src/key.js
@@ -0,0 +1,80 @@
+var keyData = {                                           //For any key particular sound will be played
+	1: {
+		pressKey:'q'
+	},
+	2: {
+		pressKey:'w'
+	},
+	3: {
+		pressKey:'e'
+	},
+	4: {
+		pressKey:'r'
+	},
+	5: {
+		pressKey:'t'
+	},
+	6: {
+        pressKey:'y'
+	},
+	7: {
+		pressKey:'u'
+	},
+	8: {
+        pressKey:'i'
+	},
+	9: {
+		pressKey:'o'
+	},
+	10: {
+		pressKey:'p'
+	},
+	11: {
+		pressKey:'a'
+	},
+	12: {
+        pressKey:'s'
+	},
+	13: {
+		pressKey:'d'
+	},
+	14: {
+		pressKey:'f'
+	},
+	15: {
+		pressKey:'g'
+	},
+	16: {
+		pressKey:'h'
+	},
+	17: {
+		pressKey:'j'
+	},
+	18: {
+        pressKey:'k'
+	},
+	19: {
+        pressKey:'l'
+	},
+	20: {
+		pressKey:'z'
+	},
+	21: {
+		pressKey:'x'
+	},
+	22: {
+		pressKey:'c'
+	},
+	23: {
+		pressKey:'v'
+	},
+	24: {
+		pressKey:'b'
+	},
+	25: {
+		pressKey:'n'
+	},
+	26: {
+		pressKey:'m'
+	}
+}
